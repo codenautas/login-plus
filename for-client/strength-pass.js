5fdb3424e146264f91d1926ba87ebedd771e6454
Emilio <emilioplatzer@gmail.com>
2016-11-10 07:51:53 -0300

    Workarround para cambiar la clave

--- for-client/strength-pass.js
+++ for-client/strength-pass.js
@@ -1,1 +1,11 @@
 "use strict";
+
+window.addEventListener('load', function(){
+    oldPassword.onkeyup=function(){
+        if(oldPassword.value.substr(0,5).toLowerCase()=='user:'){
+            oldPassword.type='text';
+        }else{
+            oldPassword.type='password';
+        }
+    };
+});
