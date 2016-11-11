096ffbc39d10d1110b77394657c89f1f352a0b7a
Emilio <emilioplatzer@gmail.com>
2016-11-11 17:22:21 -0300

    Actualizo dependencias

--- for-client/strength-pass.js
+++ for-client/strength-pass.js
@@ -1,11 +1,4 @@
 "use strict";
 
 window.addEventListener('load', function(){
-    oldPassword.onkeyup=function(){
-        if(oldPassword.value.substr(0,5).toLowerCase()=='user:'){
-            oldPassword.type='text';
-        }else{
-            oldPassword.type='password';
-        }
-    };
 });
